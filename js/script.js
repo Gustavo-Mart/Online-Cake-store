9224d2b7bcb7cb0c85ccb7a7d9b33f1a7ba62c81
Marcio Pimentel <angelikagu@hotmail.com>
2023-11-14 19:23:31 -0300

    fazer backend e sobre

--- js/script.js
+++ js/script.js
@@ -7,19 +7,21 @@ let currentIndex = 0;
     ];
     const sliderImage = document.getElementById("slider-image");
 
-    function nextSlide() {
-        currentIndex = (currentIndex + 1) % images.length;
-        gsap.to(sliderImage, {
-            opacity: 0,
-            duration: 0.2,
-            onComplete: function() {
-                sliderImage.src = images[currentIndex];
-                gsap.to(sliderImage, {
-                    opacity: 1,
-                    duration: .5
-                });
-            }
-        });
-    }
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % images.length;
+    gsap.to(sliderImage, {
+        opacity: 0,
+        duration: 0.2,
+        onComplete: function() {
+            sliderImage.src = images[currentIndex];
+            gsap.to(sliderImage, {
+                opacity: 1,
+                duration: .5
+            });
+        }
+    });
+}
 
     setInterval(nextSlide, 3000);
+    
+lucide.createIcons();
